0b46aa790f8d1fe6175d01e6e792e00497676491
Chanuka Senevirathne <chanukasenevirathne24@gmail.com>
2024-09-02 22:41:45 +0530

    Add files via upload

---
+++ StringB.java
@@ -0,0 +1,41 @@
+package Stringspack;
+
+public class StringB {
+
+	public static void main(String[] args) {
+		
+		
+		StringBuffer buffer = new StringBuffer("sllit");
+		buffer.append("   Malabe Center, Kandy road");
+		
+		System.out.println(buffer);
+		
+		
+		System.out.println("-------------------------------");
+		
+		
+		buffer.reverse();
+		
+		System.out.println(buffer);
+		
+		buffer.reverse();
+		
+		System.out.println(buffer);
+
+		
+		System.out.println("-------------------------------");
+		
+		
+		
+
+		StringBuilder Sb= new StringBuilder("abc");
+		Sb.append("def");
+		
+ 
+		System.out.println(Sb);
+
+		
+		
+	}
+
+}
